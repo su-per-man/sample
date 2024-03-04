93835ac9bd4923b989c94c3de0f6e1f27a9e3b65
Suman <suman98kumar@gmail.com>
2024-03-03 19:46:07 -0500

    conver to server.ts

--- dist/server.js
+++ dist/server.js
@@ -15,4 +15,4 @@ app.get('/ping', (_req, res) => {
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=server.js.map
